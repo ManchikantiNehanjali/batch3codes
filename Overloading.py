2f071f8c041979b7893127a713e91a01e544bd60
ManchikantiNehanjali <84323699+ManchikantiNehanjali@users.noreply.github.com>
2023-05-18 23:20:21 +0530

    Create Overloading.py

---
+++ Overloading.py
@@ -0,0 +1,18 @@
+#Overloading in python is done by two ways by using default arguments and *args and **kwargs arguments
+class A:
+    def m1(self,*args):
+        if len(args)==1:
+            print("args len 1")
+        elif len(args)==2:
+            print("args len 2")
+obj=A()
+obj.m1(1,2)
+obj.m1(1)
+
+class A:
+    def m1(self,a=None,b=None):
+        print(a,b)
+obj=A()
+obj.m1()
+obj.m1(1)
+obj.m1(1,2)
